b76d6a39132e4416fc6b07534354c7eaaf9fdb71
George Lynch <osu4life75@gmail.com>
2024-06-02 18:41:30 -0500

    Set up
    Set up of project through finish

---
+++ color-generator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RandomColor from './components';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Random color component */}
+      <RandomColor/>
+    </div>
+  );
+}
+
+export default App;
